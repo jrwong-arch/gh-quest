94f10c0593968208179b0cf774299920219df706
Jonathan <jrwong158@gmail.com>
2025-04-12 20:47:56 +0100

    Selection Tutorial Implemented

--- BaseWatcherComponent.cs
+++ BaseWatcherComponent.cs
@@ -23,8 +23,8 @@ namespace gh_quest
     {
         //************************** GLOBAL VARIABLES **************************//
         public static string Id { get; set; } = "584cb7bd-05fd-4e50-a7de-d6e47bdf4c4f";
-        public string _ActiveScriptJSON;
-        public string _TutorialJSON;
+        public List<string> _TutorialsList = new List<string>();
+        public int _SelectedTutorialIndex = 0;
 
         public TutorialClass _ActiveTutorial { get; set; }
 
@@ -36,6 +36,7 @@ namespace gh_quest
             "GH Quest", "Primary")
 
         {
+            LoadTutorials();
         }
 
         public override void CreateAttributes()
@@ -64,6 +65,7 @@ namespace gh_quest
 
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
+            LoadTutorials();
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -76,12 +78,10 @@ namespace gh_quest
 
         protected override void BeforeSolveInstance()
         {
-
         }
 
         protected override void AfterSolveInstance()
         {
-
         }
 
 
@@ -98,7 +98,7 @@ namespace gh_quest
 
         public void SelectLesson()
         {
-            
+            OpenSelectionForm();
         }
 
         public void LaunchGHQuest()
@@ -200,6 +200,79 @@ namespace gh_quest
                     await webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
                 }
             }
+        }
+
+        //Method to Open Lesson Selection Window
+        public void OpenSelectionForm()
+        {
+            int windowWidth = 400;
+            int windowHeight = 100;
+
+            Eto.Forms.Form lessonForm = new Eto.Forms.Form
+            {
+                Title = "Select Lesson",
+                Padding = new Eto.Drawing.Padding(10),
+                Size = new Eto.Drawing.Size(windowWidth, windowHeight),
+                Resizable = false,
+                MovableByWindowBackground = true,
+                BackgroundColor = Eto.Drawing.Color.FromArgb(245,245,245,255),
+                WindowStyle = Eto.Forms.WindowStyle.Utility,
+                Topmost = true,
+                Location = new Eto.Drawing.Point((int)(Eto.Forms.Mouse.Position.X - (windowWidth / 2.0)), (int)(Eto.Forms.Mouse.Position.Y - (windowHeight / 2.0))),
+            };
+
+            Eto.Forms.Button selectButton = new Eto.Forms.Button
+            {
+                Text = "Select",
+                TextColor = Eto.Drawing.Colors.Black,
+                Size = new Eto.Drawing.Size(25, 15),
+                BackgroundColor = Eto.Drawing.Colors.White,
+            };
+
+            selectButton.Click += (object sender, EventArgs e) =>
+            {
+                GH_Document doc = OnPingDocument();
+                doc.ScheduleSolution(100, selectLesson => 
+                {
+                    //Load Tutorial Stuff
+                });
+            };
+
+            Eto.Forms.DropDown dropDown = new Eto.Forms.DropDown
+            {
+                TextColor = Eto.Drawing.Colors.Black,
+                DataStore = _TutorialsList,
+                SelectedIndex = _SelectedTutorialIndex,
+            };
+
+            dropDown.SelectedIndexChanged += (object sender, EventArgs e) =>
+            {
+                int selectedIndex = dropDown.SelectedIndex;
+                GH_Document doc = OnPingDocument();
+                doc.ScheduleSolution(100, selectLesson => 
+                {
+                    _SelectedTutorialIndex = selectedIndex;
+                });
+            };
+
+            TableLayout layout = new TableLayout
+            {
+                Padding = new Eto.Drawing.Padding(0),
+                Spacing = new Eto.Drawing.Size(10, 5),
+
+                Rows = 
+                {
+                    new Eto.Forms.TableRow(dropDown, selectButton)
+                }
+            };
+
+            lessonForm.Content = layout;
+            lessonForm.Show();
+        }
+
+        public void LoadTutorials()
+        {
+            _TutorialsList = LoadTutorial.GetAllTutorialNames("C:\\Users\\jonathan.wong\\gh-quest\\GH_Beginner_Course_Pack\\tutorials.json");
         }
 
 
