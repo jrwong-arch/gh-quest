57dfb8ff1b4000a81eb84c730a795d8b48fe168c
Jonathan <jrwong158@gmail.com>
2025-04-12 16:46:16 +0100

    Base Watcher Update

--- BaseWatcherComponent.cs
+++ BaseWatcherComponent.cs
@@ -1,9 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Eto.Forms;
 using GH_IO.Serialization;
 
 using Grasshopper;
 using Grasshopper.Kernel;
+using Rhino;
 using Rhino.Geometry;
 
 namespace gh_quest
@@ -26,7 +34,7 @@ namespace gh_quest
 
         public override void CreateAttributes()
         {
-            m_attributes = new BaseWatcherAttributes(this, null);
+            m_attributes = new BaseWatcherAttributes(this, LaunchGHQuest);
         }
 
         public override bool Write(GH_IWriter writer)
@@ -82,7 +90,96 @@ namespace gh_quest
 
         //************************** CUSTOM METHODS **************************//
 
+        public void LaunchGHQuest()
+        {
+            OpenWebPanel();
+            StartWebSocketServer();
+        }
 
+
+        // Method to open an Eto web panel
+        public void OpenWebPanel()
+        {
+            RhinoApp.WriteLine("Opening web panel...");
+            var webView = new WebView
+            {
+                Url = new Uri("http://localhost:5173/"), // Replace with your desired URL
+                Size = new Eto.Drawing.Size(800, 600)
+            };
+
+            var dialog = new Dialog
+            {
+                Title = "Web Panel",
+                ClientSize = new Eto.Drawing.Size(800, 600),
+                Content = webView
+            };
+
+            dialog.ShowModal();
+        }
+
+        // Method to start a WebSocket server
+        public async void StartWebSocketServer()
+        {
+            RhinoApp.WriteLine("Starting WebSocket server...");
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8181/ws/");
+            httpListener.Start();
+
+            RhinoApp.WriteLine("WebSocket server started at ws://localhost:8181/ws/");
+
+            while (true)
+            {
+                HttpListenerContext context = await httpListener.GetContextAsync();
+
+                if (context.Request.IsWebSocketRequest)
+                {
+                HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+                WebSocket webSocket = webSocketContext.WebSocket;
+
+                RhinoApp.WriteLine("Client connected!");
+
+                await HandleWebSocketConnection(webSocket);
+                }
+                else
+                {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+                }
+            }
+        }
+
+        private async Task HandleWebSocketConnection(WebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024];
+
+            // Send an initial message to the client
+            // Read the dummy.json file into a string
+            string filePath = "/Users/ammarnaqvi/Code/Ammar/gh-quest/dummy.json";
+            string jsonContent = System.IO.File.ReadAllText(filePath);
+            byte[] initialBuffer = Encoding.UTF8.GetBytes(jsonContent);
+            await webSocket.SendAsync(new ArraySegment<byte>(initialBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                RhinoApp.WriteLine("Client disconnected!");
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+                }
+                else
+                {
+                string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                RhinoApp.WriteLine($"Received: {message}");
+
+                // Echo the message back to the client
+                byte[] responseBuffer = Encoding.UTF8.GetBytes($"Echo: {message}");
+                await webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+        }
 
 
 
